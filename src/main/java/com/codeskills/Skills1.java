4ae784ffb4e82c5c7062edf64b203f4669b76dcd
chuckma <robbincen@163.com>
2018-11-07 16:36:04 +0800

    code skills

---
+++ src/main/java/com/codeskills/Skills1.java
@@ -0,0 +1,33 @@
+package com.codeskills;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Administrator
+ */
+public class Skills1 {
+
+
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("1");
+        list.add("2");
+        list.add("3");
+        list.add("4");
+        // list.fori
+        for (int i = 0; i < list.size(); i++) {
+
+        }
+        // list.for
+        for (String s : list) {
+
+        }
+        // list.sout
+        System.out.println(list);
+
+        // list.return
+        // return list;
+
+    }
+}
