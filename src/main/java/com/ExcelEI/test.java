b9be128d0a2df79149b60529037875bf101e118f
lucas ma <robbincen@163.com>
2018-11-19 23:13:10 +0800

    excel

--- src/main/java/com/ExcelEI/test.java
+++ src/main/java/com/ExcelEI/test.java
@@ -22,7 +22,7 @@ import java.util.Scanner;
 public class test {
 
     public static void main(String[] args) throws FileNotFoundException  {
-        String filePath = "/Users/lucasma/DeskTop/test.xls";
+        /*String filePath = "/Users/lucasma/DeskTop/test.xls";
 
         ImportParams params = new ImportParams();
         params.setTitleRows(1);
@@ -31,9 +31,26 @@ public class test {
         List<Student> list = ExcelImportUtil.importExcel(
                 new File("/Users/lucasma/DeskTop/test.xls"),
                 Student.class, params);
-        System.out.println(new Date().getTime() - start);
+//        System.out.println(new Date().getTime() - start);
         System.out.println(list.size());
-        System.out.println(ReflectionToStringBuilder.toString(list.get(1).getName()));
+        System.out.println(ReflectionToStringBuilder.toString(list.get(0).getName()));*/
+
+        test1();
+    }
+
+    public static void test1(){
+        String filePath = "/Users/lucasma/DeskTop/ExcelExportMsgClient.xlsx";
+
+        ImportParams params = new ImportParams();
+        params.setTitleRows(1);
+        params.setHeadRows(1);
+        long start = new Date().getTime();
+        List<MsgClient> list = ExcelImportUtil.importExcel(
+                new File(filePath),
+                MsgClient.class, params);
+        System.out.println(new Date().getTime() - start);
+//        Assert.assertEquals(100,list.size());
+        System.out.println(ReflectionToStringBuilder.toString(list.get(0)));
     }
 
 
