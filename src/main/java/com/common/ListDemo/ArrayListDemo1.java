69b68c9c4eeb6b3c31cda366c5662718d62bf828
chuckma <robbincen@163.com>
2018-12-11 17:24:57 +0800

    list empty

--- src/main/java/com/common/ListDemo/ArrayListDemo1.java
+++ src/main/java/com/common/ListDemo/ArrayListDemo1.java
@@ -10,8 +10,23 @@ public class ArrayListDemo1 {
 
 
     public static void main(String[] args) {
-        System.out.println(Integer.MAX_VALUE>>1);
-        List<String> list = new ArrayList<>();
+       // System.out.println(Integer.MAX_VALUE>>1);
+       //  List<String> list = new ArrayList<>();
+
+
+        List<String> list1 = new ArrayList<>();
+        System.out.println(list1.size());
+        System.out.println(list1.isEmpty());
+        System.out.println("==============");
+//        list1.add("1");
+        System.out.println(list1.size());
+        System.out.println(list1.isEmpty());
+        //list1.add(null);
+        list1.add("12");
+        System.out.println(list1.size());
+        System.out.println(!list1.isEmpty());
+        System.out.println(!list1.isEmpty()&& list1.size()>0);
+        System.out.println("==============");
 
     }
 }
