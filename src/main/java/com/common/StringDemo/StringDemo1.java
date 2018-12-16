48c9c5b9a614c944073122eb0a7a42803656dbd4
lucas ma <robbincen@163.com>
2018-12-16 11:52:10 +0800

    string

--- src/main/java/com/common/StringDemo/StringDemo1.java
+++ src/main/java/com/common/StringDemo/StringDemo1.java
@@ -10,5 +10,13 @@ public class StringDemo1 {
         String str = "1231";
         str += "45678";
         System.out.println(str);
+
+
+        String str1 = "a,b,c,,";
+        String[] arr = str1.split(",");
+        for (String s : arr) {
+            System.out.println(s);
+        }
+        System.out.println(arr.length);
     }
 }
