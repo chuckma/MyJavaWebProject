e3df758fd5f894554b97d4638159222c0ac71c84
lucas ma <robbincen@163.com>
2019-02-24 16:17:32 +0800

    Collections

--- src/main/java/com/common/collection/demo1.java
+++ src/main/java/com/common/collection/demo1.java
@@ -3,6 +3,7 @@ package com.common.collection;
 import org.apache.commons.collections.CollectionUtils;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -34,5 +35,32 @@ public class demo1 {
         // 求 list 和 list1 的交集
         System.out.println("list 和 list1 的交集是： "+CollectionUtils.intersection(list, list1));
 
+        System.out.println("-------------------");
+        test1();
     }
+
+
+    private static void test1() {
+
+        /**
+         * singleton ,singletonList ,singletonMap 返回的容器都是不可修改的
+         * 如果调用修改接口 会抛出 UnsupportedOperationException
+         */
+        List<Integer> list = Collections.singletonList(1);
+        System.out.println(list.contains(1));
+
+        // 下面两行会抛出异常
+//        list.add(3);
+//        list.clear();
+        System.out.println(list);
+
+        /**
+         * emptyList，emptySet emptyMap 返回的集合都是不可修改的，
+         * 如果调用修改接口，也会抛出 UnsupportedOperationException
+         */
+        List<Object> objects = Collections.emptyList();
+//        objects.add(2);
+        System.out.println(objects);
+    }
+
 }
