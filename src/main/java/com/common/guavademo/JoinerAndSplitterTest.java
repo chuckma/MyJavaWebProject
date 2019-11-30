c92f5722c583f1450be74f392b8886760cb44a0e
lucas ma <robbincen@163.com>
2019-11-30 14:16:14 +0800

    Guava Joiner Test

--- src/main/java/com/common/guavademo/JoinerAndSplitterTest.java
+++ src/main/java/com/common/guavademo/JoinerAndSplitterTest.java
@@ -2,7 +2,10 @@ package com.common.guavademo;
 
 import com.google.common.base.Joiner;
 import com.google.common.base.Splitter;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Lists;
 
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
@@ -14,18 +17,24 @@ import java.util.Map;
  * Joiner 方法测试
  **/
 
-public class JoinerTest {
+public class JoinerAndSplitterTest {
 
 
-    public static void main(String[] args) {
+    private static final List<String> list1 = Arrays.asList
+            ("google","guava","java","scala","kafka");
+
+    private static final List<String> list2 = Arrays.asList
+            ("google","guava","java",null,"scala","kafka");
+
+    public static void main(String[] args) throws IOException {
 
 
-//        testJoiner1();
-        testGuavaSplitterAllTypeMethod();
+        testJoiner1();
+//        testGuavaSplitterAllTypeMethod();
     }
 
 
-    public static void testJoiner1() {
+    public static void testJoiner1() throws IOException {
 
         String string = "a,bb,ccc,dddd";
         String[] strings = {"a", "b", "c"};
@@ -35,9 +44,37 @@ public class JoinerTest {
         String newStrings = Joiner.on("-").join(Arrays.asList(strings));
         System.out.println(newStrings);
 
-        Iterable<String> newString2 = Splitter.on(",").split(string);
-        newString2.forEach(System.out::println);
-        System.out.println(newString2);
+
+
+        List<Integer> list1 = Lists.newArrayList();
+        for (int i = 0; i < 10; i++) {
+            list1.add(i);
+        }
+        list1.add(null);
+        System.out.println(list1);
+        // skipNulls() 跳过 null
+        System.out.println(Joiner.on("-").skipNulls().join(list1));
+
+
+        // useForNull 替换 null
+        System.out.println(Joiner.on("-").useForNull("我原来的值是null，不过被替换成现在的了").join(list1));
+
+
+        System.out.println();
+        ImmutableMap<String, String> of = ImmutableMap.of("id", "123", "name", "green");
+
+        // withKeyValueSeparator 可以将一个 k v 结果的数据通过给定的符号链接起来
+        System.out.println(Joiner.on(";").withKeyValueSeparator("=").join(of));
+
+
+
+        //4.把集合变成一个stringBuilder 或者stringBuffer
+        StringBuilder builder = Joiner.on("#").useForNull("DEFAULT").appendTo(new StringBuilder(), list2);
+        StringBuffer buffer = Joiner.on("#").useForNull("DEFAULT").appendTo(new StringBuffer(), list2);
+        System.out.println("拼接成的 builder是："+builder);
+        System.out.println("拼接成的buffer是："+buffer);
+
+
     }
 
     public static void testGuavaSplitterAllTypeMethod() {
@@ -85,6 +122,14 @@ public class JoinerTest {
         String str = "xiaoming=11,xiaohong=23";
         Map<String,String> map = Splitter.on(",").withKeyValueSeparator("=").split(str);
         System.out.println("String 转 map"+map);
+
+
+
+        // guava 支持多个字符串的分割，或者特定正则的分割
+        String input = "aa.。.dd,,ff,,.,.,../";
+        List<String> result1 = Splitter.onPattern("[.|,|/|。]").omitEmptyStrings().splitToList(input);
+
+        System.out.println("多个字符串分割"+result1);
 
     }
 }
